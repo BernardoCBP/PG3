e7a0c79fe13caae70b1cae3e173fb8cff3283dc2
Bernardo Pineda <bernardocb.pineda@gmail.com>
2021-03-30 12:30:13 +0100

    Trab1_Q1

---
+++ PG3_Trabs/src/trab1/grupo1/Player.java
@@ -0,0 +1,61 @@
+package trab1.grupo1;
+
+import java.util.Objects;
+
+public class Player {
+    // Constante
+    public static final int MIN_GAMES = 0;
+    public static final int MIN_POINTS = 0;
+    //Variaveis de instancia
+    private final String name;
+    private int numQuizzes, totalQuizPoints;
+
+    //Construtores
+    public Player(String nm, int nq, int tqs){
+        this.name = nm;
+        this.numQuizzes = nq;
+        this.totalQuizPoints = tqs;
+    }
+
+    public Player(String toString){
+        setNumQuizzes( toString.substring(0, toString.indexOf(':') ) );
+        setPoints( toString.substring(toString.indexOf(':')+1, toString.indexOf('-') ) );
+        name = toString.substring( toString.indexOf('-')+1 ).trim();
+    }
+
+    //Getters
+    public String getName() { return name; }
+    public int getNumQuizzes() { return numQuizzes; }
+    public int getPoints() { return totalQuizPoints; }
+
+    //Setters
+    public void setNumQuizzes( int numQuizzes ) {
+        if( numQuizzes >= MIN_GAMES)
+            this.numQuizzes = numQuizzes;
+    }
+    public void setPoints( int totalQuizPoints ) {
+        if( totalQuizPoints >= MIN_POINTS)
+            this.totalQuizPoints = totalQuizPoints;
+    }
+    public void setNumQuizzes( String str) {
+        setNumQuizzes(Integer.parseInt( str.trim() ));
+    }
+    public void setPoints( String str) {
+        setPoints(Integer.parseInt( str.trim() ));
+    }
+
+    //Metodos de instancia
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;                                  //verificar se os objetos referenciam o mesmo objeto
+        if (o == null || getClass() != o.getClass()) return false;   //verificar se são da mesma classe
+        Player player = (Player) o;                                  //como sabemos que são da mesma classe realizamos um cast
+        return numQuizzes == player.numQuizzes && totalQuizPoints == player.totalQuizPoints && name.equals(player.name);
+    }
+    @Override
+    public String toString() {
+        return numQuizzes + ":" + totalQuizPoints + " - " + name;
+    }
+
+}
+
---
+++ PG3_Trabs/src/trab1/grupo1/Print.java
@@ -0,0 +1,18 @@
+package trab1.grupo1;
+
+public class Print {
+    public static void main(String[] args) {
+        Player p1 = new Player ("aa", 1, 10 );
+        System.out.println( p1.toString() );
+        Player p2 = new Player ("1:10 - aa");
+        System.out.println( p1 == p2 );
+        System.out.println( p1.equals(p2) );
+        Player p3 = p1;
+        System.out.println( p1.equals(p3) );
+        if ( p3 != null )
+            System.out.println( p1==p3 );
+
+        System.out.println(p1);
+        System.out.println( p2.getName() + " " + p2.getNumQuizzes() + " " + p2.getPoints() );
+    }
+}
