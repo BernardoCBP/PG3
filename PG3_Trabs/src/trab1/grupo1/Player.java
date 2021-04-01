80e949e745942e367dd44500814ad450364ae980
Bernardo Pineda <bernardocb.pineda@gmail.com>
2021-04-01 22:14:25 +0100

    Trab1_3

--- PG3_Trabs/src/trab1/grupo1/Player.java
+++ PG3_Trabs/src/trab1/grupo1/Player.java
@@ -1,8 +1,9 @@
 package trab1.grupo1;
 
+import java.util.Arrays;
 import java.util.Objects;
 
-public class Player {
+public class Player implements Comparable<Player> {
     // Constante
     public static final int MIN_GAMES = 0;
     public static final int MIN_POINTS = 0;
@@ -11,22 +12,22 @@ public class Player {
     private int numQuizzes, totalQuizPoints;
 
     //Construtores
-    public Player(String nm, int nq, int tqs){
-        this.name = nm;
-        this.numQuizzes = nq;
-        this.totalQuizPoints = tqs;
+    public Player(String name, int numQuizzes, int totalQuizPoints){
+        this.name = name;
+        setNumQuizzes(numQuizzes);
+        setTotalPoints(totalQuizPoints);
     }
 
     public Player(String toString){
         setNumQuizzes( toString.substring(0, toString.indexOf(':') ) );
-        setPoints( toString.substring(toString.indexOf(':')+1, toString.indexOf('-') ) );
+        setTotalPoints( toString.substring(toString.indexOf(':')+1, toString.indexOf('-') ) );
         name = toString.substring( toString.indexOf('-')+1 ).trim();
     }
 
     //Getters
     public String getName() { return name; }
     public int getNumQuizzes() { return numQuizzes; }
-    public int getTotalPoints() { return totalQuizPoints; }
+    public int getTotalQuizPoints() { return totalQuizPoints; }
 
 
     //Setters
@@ -34,15 +35,15 @@ public class Player {
         if( numQuizzes >= MIN_GAMES)
             this.numQuizzes = numQuizzes;
     }
-    public void setPoints( int totalQuizPoints ) {
+    public void setTotalPoints( int totalQuizPoints ) {
         if( totalQuizPoints >= MIN_POINTS)
             this.totalQuizPoints = totalQuizPoints;
     }
     public void setNumQuizzes( String str) {
         setNumQuizzes(Integer.parseInt( str.trim() ));
     }
-    public void setPoints( String str) {
-        setPoints(Integer.parseInt( str.trim() ));
+    public void setTotalPoints( String str) {
+        setTotalPoints(Integer.parseInt( str.trim() ));
     }
 
     //Metodos de instancia
@@ -54,10 +55,43 @@ public class Player {
         totalQuizPoints += quizPoints;
     }
     public int compareTo(Player p) {
-        
+        return this.totalQuizPoints - p.totalQuizPoints;
     }
 
     //Metodos estaticos
+    public static Player getPlayer(Player[] pArray, String nm) {
+        for (Player player : pArray)
+            if (player.name.equals(nm))
+                return player;
+
+        return new Player("0:0 - " + nm);
+    }
+
+    public static Player minimum( Player ... p ) {
+        if(p == null || p.length == 0)
+            return null;
+        Player min = p[0];
+        for(int i=0; i < p.length - 1 ; i++) {
+            if( min.compareTo( p[i+1] ) > 0 ) {
+                min = p[i+1];
+            }
+        }
+        return min;
+    }
+
+    public static Player[] getTop10(Player[] pArray) {
+        Player[] top10Array = new Player[10];
+
+        Arrays.sort( pArray );
+
+        for(int i = 0; i < 10 && i < pArray.length; i++) {
+            top10Array[i] = pArray[pArray.length-1-i];
+        }
+
+        top10Array = Arrays.copyOf( top10Array, Math.min(pArray.length, 10) );
+
+        return top10Array;
+    }
 
     @Override
     public boolean equals(Object o) {
@@ -72,4 +106,19 @@ public class Player {
     }
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
--- PG3_Trabs/src/trab1/grupo1/Print.java
+++ PG3_Trabs/src/trab1/grupo1/Print.java
@@ -4,16 +4,28 @@ public class Print {
     public static void main(String[] args) {
         Player p1 = new Player ("aa", 1, 10 );
         System.out.println( p1.toString() );
-        Player p2 = new Player ("1:10 - aa");
+        Player p2 = new Player ("1:0 - aa");
         System.out.println( p1 == p2 );
         System.out.println( p1.equals(p2) );
         Player p3 = p1;
         System.out.println( p1.equals(p3) );
         if ( p3 != null )
             System.out.println( p1 == p3 );
-
+        /*
         System.out.println(p1);
         System.out.println( p2.getName() + " " + p2.getNumQuizzes() + " " + p2.getTotalPoints());
-        System.out.println("Points:" + p2.getPoints());
+        System.out.println("Points:" + p1.compareTo(p2));
+        */
+        Player[] array = {
+                new Player("aa", 1, 10),
+                new Player("bb", 1, 1),
+                new Player("cc", 1, 5),
+                new Player("dd", 1, 56)
+        };
+
+        Player p4 = Player.getPlayer(array, "Juan" );
+        System.out.println(p4);
+
+        System.out.println("Min: " + Player.minimum(array[0], array[1], array[2], array[3]));
     }
 }
